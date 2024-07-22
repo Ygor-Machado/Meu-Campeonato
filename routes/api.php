f20ed338a19df73c0fd9612025fe6f5ac50026b7
Ygor-Machado <ygaounderground011@gmail.com>
2024-07-22 15:38:08 -0300

    feat: cria rotas para os controllers

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\CampeonatoController;
+use App\Http\Controllers\PartidaController;
+use App\Http\Controllers\TimeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('times', TimeController::class);
+Route::apiResource('campeonatos', CampeonatoController::class);
+Route::apiResource('partidas', PartidaController::class);
